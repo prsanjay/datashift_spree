81c58d1586c96bf6b2e3638df10cf75a5c9bf783
Sanjay Prajapati <prsanjay@users.noreply.github.com>
2017-10-26 13:28:25 +0530

    Update product_populator.rb

--- lib/loaders/spree/product_populator.rb
+++ lib/loaders/spree/product_populator.rb
@@ -11,7 +11,7 @@ module DataShift
 
     class ProductPopulator < Populator
 
-      include SpreeLoading
+      include Spree::SpreeLoading
 
       include DataShift::Logging
       extend DataShift::Logging
